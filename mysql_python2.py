d4a2142b55a2cb224db9683c45c91065c019e60a
Faizee Abdul Aleem <faizeealeem9490@gmail.com>
2026-08-12 22:47:16 +0530

    File Modified

--- mysql_python2.py
+++ mysql_python2.py
@@ -12,9 +12,9 @@ else:
     print("Connection Established")
     mycursor=conn.cursor()
     #mycursor.execute("CREATE DATABASE school")
-    mycursor.execute("SHOW DATABASES")
-    for c in mycursor.fetchall():
-        print(c)
+    #mycursor.execute("SHOW DATABASES")
+    #for c in mycursor.fetchall():
+        #print(c)
 
     q='''
     CREATE TABLE students(
@@ -32,9 +32,48 @@ else:
     subject VARCHAR(20)
     )
     '''
-    #mycursor.execute(q1)
-    mycursor.execute("SHOW TABLES")
-    for c in mycursor.fetchall():
-        print(c)
+    q2='''
+    CREATE TABLE subject(
+    subjectname VARCHAR(20),
+    subjectsmarks INT(10)
+    )
+    '''
+    #mycursor.execute(q)
+    #mycursor.execute("SHOW TABLES")
+    #for x in mycursor.fetchall():
+        #print(x)
 
-    
+    q3='''
+    INSERT INTO students(roll_no,fullname,gender,marks) values(%s,%s,%s,%s)
+    '''
+    v=(33,"Ali","Male",467)
+    v1=(43,"Yasmeen","Female",500)
+    v2=(31,"Musaddique","Male",300)
+    v3=(50,"Farhan","Male",550)
+    v4=(56,"Izhan","Male",580)
+
+    vlist=[
+        (12,"Musaddique Seth","Male",350),
+        (13,"Musaddique Bade Seth","Male",420),
+        (15,"Farhan Seth","Male",550),
+        (14,"Abdullah","Male",350),
+        (16,"Sarim","Male",450),
+        (17,"Ibrahim","Male",380),
+        (18,"Saad","Male",710),
+        (19,"Naif","Male",690)
+    ]
+    #mycursor.execute(q3,v)
+    conn.commit()
+    #mycursor.execute(q3,v1)
+    conn.commit()
+    #mycursor.execute(q3,v2)
+    conn.commit()
+    #mycursor.execute(q3,v3)
+    conn.commit()
+    #mycursor.execute(q3,v4)
+    conn.commit()
+    mycursor.executemany(q3,vlist)
+    conn.commit()
+    mycursor.execute("SELECT * FROM students")
+    for x in mycursor.fetchall():
+        print(x)
