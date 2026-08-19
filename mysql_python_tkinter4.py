c4249b2abf1ba0fcfd2d7feb0c5e0dc65f218d99
Faizee Abdul Aleem <faizeealeem9490@gmail.com>
2026-08-19 20:56:49 +0530

    File Update

--- mysql_python_tkinter4.py
+++ mysql_python_tkinter4.py
@@ -1,9 +1,6 @@
 import mysql.connector
 from tkinter import *
-<<<<<<< HEAD
 from tkinter import messagebox
-=======
->>>>>>> cb3666d65ff41ef5923c35fb7ffbfd6689f91667
 try:
     conn=mysql.connector.connect(
         host="localhost",
@@ -16,7 +13,6 @@ except:
 else:
     print("Connection Successful!")
     mycursor=conn.cursor()
-<<<<<<< HEAD
     #mycursor.execute("SELECT * FROM students")
     #for c in mycursor.fetchall():
         #print(c)
@@ -44,15 +40,12 @@ else:
             L7.config(text=r1[0][1])
             L8.config(text=r1[0][2])
             L9.config(text=r1[0][3])
-=======
     mycursor.execute("SELECT * FROM students")
     for c in mycursor.fetchall():
         print(c)
     
-    def display_record():
-        print(r.get())
+
         
->>>>>>> cb3666d65ff41ef5923c35fb7ffbfd6689f91667
     root=Tk()
     root.title("Display Data")
     r=StringVar()
@@ -79,7 +72,6 @@ else:
     L5.grid(row=1,column=3,padx=5,pady=5)
 
     #Display Record
-<<<<<<< HEAD
     L6=Label(root,text="",font=("Arial",11,"bold"),padx=5,pady=5)
     L6.grid(row=2,column=0,padx=5,pady=5)
 
@@ -90,17 +82,6 @@ else:
     L8.grid(row=2,column=2,padx=5,pady=5)
     
     L9=Label(root,text="",font=("Arial",11,"bold"),padx=5,pady=5)
-=======
-    L6=Label(root,text="1",font=("Arial",11,"bold"),padx=5,pady=5)
-    L6.grid(row=2,column=0,padx=5,pady=5)
-
-    L7=Label(root,text="Ali",font=("Arial",11,"bold"),padx=5,pady=5)
-    L7.grid(row=2,column=1,padx=5,pady=5)
-
-    L8=Label(root,text="Male",font=("Arial",11,"bold"),padx=5,pady=5)
-    L8.grid(row=2,column=2,padx=5,pady=5)
-    
-    L9=Label(root,text="450",font=("Arial",11,"bold"),padx=5,pady=5)
->>>>>>> cb3666d65ff41ef5923c35fb7ffbfd6689f91667
     L9.grid(row=2,column=3,padx=5,pady=5)
+    
     root.mainloop()
